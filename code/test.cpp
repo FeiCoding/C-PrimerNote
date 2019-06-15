0cc8970e3830547995a8b79d95dc81c69cb66c4e
FeiCoding <254356151@qq.com>
2019-06-15 12:55:35 +0100

    update files

---
+++ code/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string ss;
+    while(cin >> ss){
+        cout << "This is a c++ test" << endl;
+        cout << ss << endl;
+    }
+    return 0;
+}
