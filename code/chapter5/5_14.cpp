8e522c3ae693529f1edc332f183e5bbdcce23f0d
FeiCoding <254356151@qq.com>
2019-06-22 16:58:49 +0100

    update chapter 5

---
+++ code/chapter5/5_14.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int mian(){
+    cout << "Input words you want to compare: (Input end to end this program)" << endl;
+    string old_str;
+    string new_str ;   vector<string> vs;
+    vector<int> vi;
+    while(cin >> str){
+        
+        else{
+            vs.push_back(str);
+            if(vs.)
+        }
+    }
+    
+}
