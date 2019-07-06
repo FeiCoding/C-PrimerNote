f3934c9d99534bbe9e57091775939e5f8ec23e3a
FeiCoding <254356151@qq.com>
2019-07-06 18:17:21 +0100

    update chapter 7

--- code/chapter7/7_4.cpp
+++ code/chapter7/7_4.cpp
@@ -4,17 +4,22 @@ using namespace std;
 
 struct Person
 {
-    string name;
-    string address;
-    string getName() const{
-        return name;
-    }
-    string getAddress() const{
-        return address;
-    }
-
-    istream & read(istream &i, Person &p);
-    ostream & print(ostream &o, const Person &p);
+    friend istream & read(istream &i, Person &p);
+    friend ostream & print(ostream &o, const Person &p);
+    
+    private: 
+        string name;
+        string address = "China";
+        
+    public:
+        string getName() const{
+            return name;
+        }
+        string getAddress() const{
+            return address;
+        }
+        Person() = default;
+        Person(string n, string ad):name(n),address(ad){ }    
 };
 
 istream &read(istream &i, Person &p){
@@ -30,7 +35,7 @@ ostream &print(ostream &o, const Person &p){
 int main(int argc, char const *argv[])
 {
     Person p;
-    p.name = "Fei Xie";
+    p.name = "Fei Xie1";
     p.address = "London, United Kingdom";
     cout << p.getName() << endl;
     cout << p.getAddress() << endl;
@@ -38,6 +43,10 @@ int main(int argc, char const *argv[])
     cout << "Please input your name:" << endl;
     read(cin, p);
     print(cout, p);
+    cout << endl;
+
+    Person p2("Fei Xie3", "London");
+    print(cout, p2);
     cout << endl;
     return 0;
 }
