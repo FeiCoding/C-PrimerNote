dd6a987bfd91264fcd0f5040bd5c98c42ebe6e9d
Fei Xie <fei.xie@factset.com>
2019-07-29 15:09:09 +0100

    update

---
+++ code/chapter4/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool a = true;
+    cout << -a << endl;
+    bool b = -a;
+    cout << b << endl;
+    return 0;
+}
